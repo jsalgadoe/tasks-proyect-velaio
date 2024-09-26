6a9c73c02eba74b722eb996ba1f954ae4c00b2ea
Jorge Andres Salgado Echeverria <jsalgadoecheverria@gmail.com>
2024-09-26 12:14:10 -0500

    add bootstrep, service tasks and listTaks standalone

---
+++ src/app/tasks/list-task/list-task.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-list-task',
+  templateUrl: './list-task.component.html',
+  styleUrls: ['./list-task.component.css'],
+})
+export class ListTaskComponent {}
